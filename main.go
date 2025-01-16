df65615034440a13fd34447715878d194ef6f7fa
jidasetima <jidasetima@gmail.com>
2025-01-15 22:57:12 -0500

    Initial commit of project with basic functionality to fetch firebase token

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	firebase "firebase.google.com/go/v4"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+	"log"
+	"net/http"
+	"os"
+)
+
+type tokenPayload struct {
+	Token string `json:"token"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file", err)
+	}
+
+	serverUrl, serverUrlIsSet := os.LookupEnv("SERVER_URL")
+
+	if serverUrlIsSet != true {
+		log.Fatalf("Error loading SERVER_URL environment variable")
+	}
+
+	router := gin.Default()
+	router.POST("/token", getToken)
+
+	router.Run(serverUrl)
+}
+
+func getToken(c *gin.Context) {
+	filename := c.PostForm("filename")
+	if filename == "" {
+		log.Print("Bad Request. Filename not provided")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Filename required"})
+		return
+	}
+
+	credentials := option.WithCredentialsFile(filename)
+	application, err := firebase.NewApp(c, nil, credentials)
+	if err != nil {
+		log.Fatalf("error initializing app: $v\n", err)
+	} else {
+		log.Print("successful connection to firebase")
+	}
+
+	client, err := application.Auth(c)
+
+	if err != nil {
+		log.Print("error getting auth: $v\n", err)
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	token, err := client.CustomToken(c, filename)
+
+	if err != nil {
+		log.Print("error getting token: $v\n", err)
+		c.JSON(http.StatusBadGateway, gin.H{"Firebase token minting failed with error": err.Error()})
+		return
+	}
+	log.Printf("Got custom token: $v\n", token)
+
+	var tokenPayload tokenPayload
+	tokenPayload.Token = token
+
+	c.JSON(http.StatusOK, tokenPayload)
+}
