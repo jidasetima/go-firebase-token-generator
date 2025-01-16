a74aa47ecf26ee323a91b82d86905ae0b60b8413
jidasetima <jidasetima@gmail.com>
2025-01-16 11:01:29 -0500

    Update process to fetch Google OAuth2 token instead of Firebase Auth token

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	firebase "firebase.google.com/go/v4"
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"google.golang.org/api/option"
+	"golang.org/x/oauth2/google"
 	"log"
 	"net/http"
 	"os"
@@ -44,33 +44,32 @@ func getToken(c *gin.Context) {
 		return
 	}
 
-	credentials := option.WithCredentialsFile(filenamePayload.Filename)
-	application, err := firebase.NewApp(c, nil, credentials)
+	// Read the file contents into memory
+	fileContent, err := os.ReadFile(filenamePayload.Filename)
 	if err != nil {
-		log.Fatalf("error initializing app: $v\n", err)
-	} else {
-		log.Print("successful connection to firebase")
-	}
-
-	client, err := application.Auth(c)
-
-	if err != nil {
-		log.Print("error getting auth: $v\n", err)
-		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		log.Print("Error reading credentials file: %v\n", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "File contents not readable"})
 		return
 	}
 
-	token, err := client.CustomToken(c, filenamePayload.Filename)
-
+	// Retrieve the token using Google OAuth2 credentials
+	credentials, err := google.CredentialsFromJSON(context.Background(), fileContent, "https://www.googleapis.com/auth/firebase.messaging")
 	if err != nil {
-		log.Print("error getting token: $v\n", err)
-		c.JSON(http.StatusBadGateway, gin.H{"Firebase token minting failed with error": err.Error()})
+		log.Print("Error loading credentials: %v\n", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "File contents not readable"})
 		return
 	}
-	log.Printf("Got custom token: $v\n", token)
+
+	// Get token from source
+	token, err := credentials.TokenSource.Token()
+	if err != nil {
+		log.Print("Error retrieving OAuth2 token: %v\n", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Not able to retrieve OAuth2 token"})
+		return
+	}
 
 	var tokenPayload tokenPayload
-	tokenPayload.Token = token
+	tokenPayload.Token = token.AccessToken
 
 	c.JSON(http.StatusOK, tokenPayload)
 }
